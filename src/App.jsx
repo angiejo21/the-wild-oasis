75e153cf7f88750a8ca0e52f387111eda2be5c62
Angela Bellò <angela.bello90@gmail.com>
2024-01-29 16:28:13 +0100

    +react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
-import styled from "styled-components";
-
-import GlobalStyles from "./styles/GlobalStyles";
-
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-const StyledApp = styled.main`
-  background-color: orangered;
-  padding: 20px;
-`;
-
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button onClick={() => console.log("Check in")}>Check in</Button>
-        <Button onClick={() => console.log("Check out")}>Check out</Button>
-
-        <Input type="number" placeholder="Number of guests" />
-      </StyledApp>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
