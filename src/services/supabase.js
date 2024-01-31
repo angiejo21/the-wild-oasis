f1485c818f567766a4b072e6ed20cfc1d74c2681
Angela Bellò <angela.bello90@gmail.com>
2024-01-31 12:12:44 +0100

    + image input/ form row component

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://gmtqmhfvhxcqydcqvrxn.supabase.co";
+export const supabaseUrl = "https://gmtqmhfvhxcqydcqvrxn.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtdHFtaGZ2aHhjcXlkY3F2cnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1NDU5ODIsImV4cCI6MjAyMjEyMTk4Mn0.UJB2IRhd04ONbrp-PxeaBP-ancFpgSZ2KtLdrmrQHF8";
 const supabase = createClient(supabaseUrl, supabaseKey);
