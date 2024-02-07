de73eafaa7ce659811d5cf59467d30b0852275d4
Angela Bellò <angela.bello90@gmail.com>
2024-02-07 16:11:29 +0100

    + login form

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -21,7 +21,14 @@ const Heading = styled.h1`
     props.as === "h3" &&
     css`
       font-size: 2rem;
-      font-weight: 6500;
+      font-weight: 500;
+    `}
+  ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-align: center;
     `}
 
   line-height:1.4;
